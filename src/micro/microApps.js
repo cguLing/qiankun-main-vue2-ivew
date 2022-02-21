9babf2fe94f2317c621959eecf1c2d69a6af8737
Fei Yan <yanfei01@rd.netease.com>
2022-02-21 09:53:55 +0800

    0221

--- src/micro/microApps.js
+++ src/micro/microApps.js
@@ -1,20 +1,12 @@
 import store from './store'
 const microApps = [
   {
-    name: 'sub-cicd',
-    entry: '//localhost:8010/subapp/sub-cicd', // process.env.VUE_APP_SUB_VUE,
+    name: 'bus',
+    entry: '//bus.site.zhidaoxuexiao.com/subapp/bus/', // process.env.VUE_APP_SUB_VUE,
     icon: 'ios-american-football-outline',
     container: '#subapp-viewport', // 子应用挂载的div
-    title: '子应用1',
-    activeRule: '/sub-cicd'
-  },
-  {
-    name: 'cicd',
-    entry: '//127.0.0.1:8020', // process.env.VUE_APP_SUB_VUE,
-    icon: 'ios-cog-outline',
-    container: '#subapp-viewport', // 子应用挂载的div
-    title: '大禹平台',
-    activeRule: '/cicd'
+    title: '业务线与服务',
+    activeRule: '/bus'
   }
 ]
 const apps = microApps.map(item => {
